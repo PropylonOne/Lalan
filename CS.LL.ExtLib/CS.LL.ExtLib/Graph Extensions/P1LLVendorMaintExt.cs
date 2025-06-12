1ca1913582272235e86a33be9dc8f7596fedd507
DhanushkaPropylonOne <saman@propylonone.com>
2025-06-12 15:40:45 +0530

    #File name Changed #P1LLAPSetupExt.cs #P1LLVendorMaint.cs

---
+++ CS.LL.ExtLib/CS.LL.ExtLib/Graph Extensions/P1LLVendorMaintExt.cs
@@ -0,0 +1,34 @@
+﻿using PX.Data;
+using PX.Objects.AP;
+using PX.Objects.CS;
+
+namespace CS.LL.ExtLib
+{
+    public class SLLVendorMaintCExt : PXGraphExtension<VendorMaint>
+    {
+
+        #region IsActive
+        public static bool IsActive()
+        {
+            return PXAccess.FeatureInstalled<FeaturesSet.inventory>();
+        }
+        #endregion
+
+        #region Event Handler
+        protected void _(Events.RowInserted<Vendor> e)
+        {
+            var row = e.Row;
+            if (row == null) return;
+
+            APSetup setup = PXSelect<APSetup>.Select(Base);
+            var setupExt = setup?.GetExtension<P1LLAPSetupExt>();
+
+            if (setupExt?.UsrP1LLIsVendorInactive == true)
+            {
+                row.VStatus = VendorStatus.Inactive;
+                e.Cache.Update(row);
+            }
+        }
+        #endregion
+    }
+}
