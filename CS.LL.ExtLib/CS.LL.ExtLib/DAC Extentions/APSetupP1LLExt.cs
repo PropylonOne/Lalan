835b958c1e712885501135b0e75a1571259076dd
DhanushkaPropylonOne <saman@propylonone.com>
2025-06-12 13:42:52 +0530

    #Vendor Inactive

---
+++ CS.LL.ExtLib/CS.LL.ExtLib/DAC Extentions/APSetupP1LLExt.cs
@@ -0,0 +1,37 @@
+﻿using PX.Data;
+using PX.Data.EP;
+using PX.Data.Licensing;
+using PX.Data.ReferentialIntegrity.Attributes;
+using PX.Objects.AP;
+using PX.Objects.CS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS.LL.ExtLib
+{
+    public sealed class APSetupP1LLExt : PXCacheExtension<PX.Objects.AP.APSetup>
+    {
+        #region IsActive
+
+        public static bool IsActive()
+        {
+            return PXAccess.FeatureInstalled<FeaturesSet.inventory>();
+        }
+
+        #endregion
+
+
+        #region UsrP1LLIsVendorInactive
+        [PXDBBool]
+        [PXUIField(DisplayName = "Create New Vendor as Inactive")]
+        [PXDefault(false, PersistingCheck = PXPersistingCheck.Nothing)]
+        public bool? UsrP1LLIsVendorInactive { get; set; }
+        public abstract class usrP1LLIsVendorInactive : PX.Data.BQL.BqlBool.Field<usrP1LLIsVendorInactive> { }
+        #endregion
+
+
+    }
+}
---
+++ CS.LL.ExtLib/CS.LL.ExtLib/Graph Extensions/VendorMaintP1LLExt.cs
@@ -0,0 +1,36 @@
+﻿using PX.Data;
+using PX.Objects.AP;
+using PX.Objects.CS;
+
+namespace CS.LL.ExtLib
+{
+    public class VendorMaintCSLLExt : PXGraphExtension<VendorMaint>
+    {
+
+        #region IsActive
+
+        public static bool IsActive()
+        {
+            return PXAccess.FeatureInstalled<FeaturesSet.inventory>();
+        }
+
+        #endregion
+
+        #region Event Handler
+        protected void _(Events.RowInserted<Vendor> e)
+        {
+            var row = e.Row;
+            if (row == null) return;
+
+            APSetup setup = PXSelect<APSetup>.Select(Base);
+            var setupExt = setup?.GetExtension<APSetupP1LLExt>();
+
+            if (setupExt?.UsrP1LLIsVendorInactive == true)
+            {
+                row.VStatus = VendorStatus.Inactive;
+                e.Cache.Update(row);
+            }
+        }
+        #endregion
+    }
+}
